f773b3ad6365bab0b3af57326f5fe39c85fc9ceb
MBMarsell <martbustmar@gmail.com>
2022-08-31 17:31:48 +0200

    add pics about for about

--- src/constants/images.js
+++ src/constants/images.js
@@ -22,6 +22,7 @@ import about01 from '../assets/about01.png';
 import about02 from '../assets/about02.png';
 import about03 from '../assets/about03.png';
 import about04 from '../assets/about04.png';
+import about05 from '../assets/about05.png';
 
 import profile from '../assets/profile.png';
 import circle from '../assets/circle.svg';
@@ -59,6 +60,7 @@ export default {
   about02,
   about03,
   about04,
+  about05,
   profile,
   circle,
   logo,
