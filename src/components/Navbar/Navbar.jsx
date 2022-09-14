939be1f233baa318c02609caf1c3d175e029debc
MBMarsell <martbustmar@gmail.com>
2022-09-14 14:48:04 +0200

    add scroll to top at nav logo

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -11,7 +11,13 @@ const Navbar = () => {
   return (
     <nav className="app__navbar">
       <div className="app__navbar-logo">
-        <img src={images.logo} alt="logo" />
+        <img
+          src={images.logo}
+          alt="logo"
+          onClick={() => {
+            window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
+          }}
+        />
       </div>
       <ul className="app__navbar-links">
         {['home', 'about', 'work', 'skills', 'contact'].map(item => (
